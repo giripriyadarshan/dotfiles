63cc55507e5efcc4cb2ff94bc390bd30e7d36a55
giripriyadarshan <43517605+giripriyadarshan@users.noreply.github.com>
2021-03-26 17:54:57 +0530

    Separate files and config for different OS

--- setup.sh
+++ setup.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
 # Update Ubuntu and get standard repository programs
-sudo apt update && sudo apt full-upgrade -y
+# sudo apt update && sudo apt full-upgrade -y
 
-# install apt packages for Ubuntu x86 Installation only --------------------- Ubuntu x86
-for f in ubuntu-x86/*.txt; do sudo apt install -y $(grep -v '^#' $f); done
-for f in ubuntu-x86/*.sh; do bash "$f" -H; done
+source menu.sh
 
-# install apt packages for Raspberry Pi Headless Installation only --------------------- RPi Headless
-for f in rpi-headless/*.txt; do sudo apt install -y $(grep -v '^#' $f); done
-for f in rpi-headless/*.sh; do bash "$f" -H; done
+case $(select_opt "* Ubuntu x86" "* RPi Headless" "* RPi Desktop" "* Ubuntu Arm Desktop" "CANCEL") in
+0)
+    # install apt packages for Ubuntu x86 Installation only --------------------- Ubuntu x86
+    for f in ubuntu-x86/*.txt; do sudo apt install -y $(grep -v '^#' $f); done
+    for f in ubuntu-x86/*.sh; do bash "$f" -H; done
+    ;;
+1)
+    # install apt packages for Raspberry Pi Headless Installation only --------------------- RPi Headless
+    for f in rpi-headless/*.txt; do sudo apt install -y $(grep -v '^#' $f); done
+    for f in rpi-headless/*.sh; do bash "$f" -H; done
+    ;;
+2)
+    # install apt packages for Raspberry Pi Desktop Installation only --------------------- RPi Desktop
+    for f in rpi-desktop/*.txt; do sudo apt install -y $(grep -v '^#' $f); done
+    for f in rpi-desktop/*.sh; do bash "$f" -H; done
+    ;;
+3)
+    # install apt packages for Ubuntu ARM Desktop Installation only --------------------- Ubuntu ARM
+    for f in ubuntu-arm-desktop/*.txt; do sudo apt install -y $(grep -v '^#' $f); done
+    for f in ubuntu-arm-desktop/*.sh; do bash "$f" -H; done
+    ;;
+4) echo "Bye .... see you later" ;;
 
-# install apt packages for Raspberry Pi Desktop Installation only --------------------- RPi Desktop
-for f in rpi-desktop/*.txt; do sudo apt install -y $(grep -v '^#' $f); done
-for f in rpi-desktop/*.sh; do bash "$f" -H; done
-
-# install apt packages for Ubuntu ARM Desktop Installation only --------------------- Ubuntu ARM
-for f in ubuntu-arm-desktop/*.txt; do sudo apt install -y $(grep -v '^#' $f); done
-for f in ubuntu-arm-desktop/*.sh; do bash "$f" -H; done
+esac
