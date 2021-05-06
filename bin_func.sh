bc449dafe185b8c28e058e506952d9c0e245df51
giripriyadarshan <43517605+giripriyadarshan@users.noreply.github.com>
2021-05-06 22:40:18 +0530

    made bin_func more compact

--- bin_func.sh
+++ bin_func.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
 for f in bin/* ; do
-	if [ $f != $line ] ; then 
-		if [ -x "$f" ] 
+		if [ ! -x "$f" ] 
 		then
-			echo "$f is already run ----"
-		else
-			echo "$f is made to run now"
 			chmod +x "$f"
 		fi
-	fi	
 done
