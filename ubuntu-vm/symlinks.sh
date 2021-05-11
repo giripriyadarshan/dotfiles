5d099635de321a045dcb1fe3b6ac5cc7ce4f79dd
giripriyadarshan <43517605+giripriyadarshan@users.noreply.github.com>
2021-05-11 09:49:49 +0530

    added cloud VM configs

---
+++ ubuntu-vm/symlinks.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+source ~/dotfiles/symfunc.sh
+
+dotfilesDir=$(pwd)
+
+# in case you want to insert a file in a folder
+# mkdir ${HOME}/.folder
+# linkDotfile .folder/.file
+
+# Link .bash_aliases for non desktop installations like rpi-headless
+
+mkdir .vim
+linkDotfile .vim
+linkDotfile .vimrc
+# linkDotfile .ackrc
+linkDotfile .bashrc
+linkDotfile .gitconfig
+# linkDotfile .tmux.conf
+# linkDotfile .goomwwmrc
+# linkDotfile .inputrc
+# linkDotfile .xinitrc
+# linkDotfile .curlrc
+# linkDotfile .gf
+linkDotfile .aliases
+linkDotfile .profile
+
+linkDotfile .cargo
+linkDotfile bin
+
+mkdir -p $dotfilesDir/.vim/bundle
+cd $dotfilesDir/.vim/bundle
+git clone git://github.com/VundleVim/Vundle.vim.git
+vim +PluginInstall +qall
+
+mkdir -p $dotfilesDir/.vim/swap
