492fd9f3f6ef839ba70d75d3883172d473466737
giripriyadarshan <43517605+giripriyadarshan@users.noreply.github.com>
2021-05-11 22:01:05 +0530

    created a separate file for VMs

---
+++ .zsh_functions/vm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+## Virtual Machines SSH
+function vm() {
+	if [ "$1" = azure-linux ]; then
+		ssh giripriyadarshan@52.163.80.54
+	elif [ "$1" = home-automation ]; then
+		if type zerotier-cli > /dev/null 2>&1 ; then
+			ssh pi@192.168.191.45
+		else
+			echo "Please install ZeroTier One"
+		fi
+	else
+		echo "Enter a valid ssh address"
+	fi
+}
+
+_vm() {
+	compadd azure-linux home-automation none
+}
+compdef _vm vm
+
