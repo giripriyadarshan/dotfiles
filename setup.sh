d46c69bff188adbd86362f028555abecb03eb71e
giripriyadarshan <43517605+giripriyadarshan@users.noreply.github.com>
2021-07-05 10:31:16 +0530

    how did i think earlier code was supposed to work?

--- setup.sh
+++ setup.sh
@@ -6,40 +6,75 @@ source menu.sh
 clear
 
 case $(select_opt "* Ubuntu x86" "* RPi Headless" "* RPi Desktop" "* Ubuntu Arm Desktop" "* Ubuntu Cloud VM" "CANCEL") in
-0)
-    # install apt packages for Ubuntu x86 Installation only --------------------- Ubuntu x86
-    sudo apt update && sudo apt full-upgrade -y
-    for f in ubuntu-x86/*.txt; do sudo apt install -y "$(grep -v '^#' "$f")"; done
-    for f in ubuntu-x86/*.sh; do bash "$f" -H; done
-    # rust takes a commandline input to continue ....
-    # so added it in the last so it won't pause other scripts
-    bash rust.sh
-    bash arduino-cli.sh
-    ;;
-1)
-    # install apt packages for Raspberry Pi Headless Installation only --------------------- RPi Headless
-    sudo apt update && sudo apt full-upgrade -y
-    for f in rpi-headless/*.txt; do sudo apt install -y "$(grep -v '^#' "$f")"; done
-    for f in rpi-headless/*.sh; do bash "$f" -H; done
-    ;;
-2)
-    # install apt packages for Raspberry Pi Desktop Installation only --------------------- RPi Desktop
-    sudo apt update && sudo apt full-upgrade -y
-    for f in rpi-desktop/*.txt; do sudo apt install -y "$(grep -v '^#' "$f")"; done
-    for f in rpi-desktop/*.sh; do bash "$f" -H; done
-    ;;
-3)
-    # install apt packages for Ubuntu ARM Desktop Installation only --------------------- Ubuntu ARM
-    sudo apt update && sudo apt full-upgrade -y
-    for f in ubuntu-arm-desktop/*.txt; do sudo apt install -y "$(grep -v '^#' "$f")"; done
-    for f in ubuntu-arm-desktop/*.sh; do bash "$f" -H; done
-    ;;
+    0)
+        # install apt packages for Ubuntu x86 Installation only --------------------- Ubuntu x86
+        sudo apt update && sudo apt full-upgrade -y
+        for f in ubuntu-x86/*.txt
+        do
+            while IFS= read -r app
+            do
+                sudo apt install -y "$app"
+            done < <(grep -v '^#' "$f")
+        done
 
-4)
-    # install apt packages for Ubuntu Cloud VM Installation only --------------------- Ubuntu Cloud VM
-    sudo apt update && sudo apt full-upgrade -y
-    for f in ubuntu-vm/*.txt; do sudo apt install -y "$(grep -v '^#' "$f")"; done
-    for f in ubuntu-vm/*.sh; do bash "$f" -H; done
-    ;;
+        for f in ubuntu-x86/*.sh; do bash "$f" -H; done
+        # rust takes a commandline input to continue ....
+        # so added it in the last so it won't pause other scripts
+        bash rust.sh
+        bash arduino-cli.sh
+        ;;
+    1)
+        # install apt packages for Raspberry Pi Headless Installation only --------------------- RPi Headless
+        sudo apt update && sudo apt full-upgrade -y
+        for f in rpi-headless/*.txt
+        do
+            while IFS= read -r app
+            do
+                sudo apt install -y "$app"
+            done < <(grep -v '^#' "$f")
+        done
 
-esac
+        for f in rpi-headless/*.sh; do bash "$f" -H; done
+        ;;
+    2)
+        # install apt packages for Raspberry Pi Desktop Installation only --------------------- RPi Desktop
+        sudo apt update && sudo apt full-upgrade -y
+        for f in rpi-desktop/*.txt
+        do
+            while IFS= read -r app
+            do
+                sudo apt install -y "$app"
+            done < <(grep -v '^#' "$f")
+        done
+
+        for f in rpi-desktop/*.sh; do bash "$f" -H; done
+        ;;
+    3)
+        # install apt packages for Ubuntu ARM Desktop Installation only --------------------- Ubuntu ARM
+        sudo apt update && sudo apt full-upgrade -y
+        for f in ubuntu-arm-desktop/*.txt
+        do
+            while IFS= read -r app
+            do
+                sudo apt install -y "$app"
+            done < <(grep -v '^#' "$f")
+        done
+
+        for f in ubuntu-arm-desktop/*.sh; do bash "$f" -H; done
+        ;;
+
+    4)
+        # install apt packages for Ubuntu Cloud VM Installation only --------------------- Ubuntu Cloud VM
+        sudo apt update && sudo apt full-upgrade -y
+        for f in ubuntu-vm/*.txt
+        do
+            while IFS= read -r app
+            do
+                sudo apt install -y "$app"
+            done < <(grep -v '^#' "$f")
+        done
+
+        for f in ubuntu-vm/*.sh; do bash "$f" -H; done
+        ;;
+
+    esac
